b4dab6b71efe1e4f26c34779932fb457f1b18110
coss <25678@i028.cc>
2022-07-01 17:03:14 +0800

    statusbar

--- script/statusbar.sh
+++ script/statusbar.sh
@@ -6,7 +6,7 @@ while true
 do
 	oVol=`amixer get Master | awk -F '[][]' 'END{print $2}'`
 	oMute=`amixer get Master | sed -n '$p' | cut -d] -f3 | cut -d[ -f2`
-	oDate=`date -d now +"%R %m/%d"`
-	xsetroot -name " / Vo:${oVol} ${oMute^^} / ${oDate} / Arch /"
+	oDate=`date -d now +"%m%d %R"`
+	xsetroot -name " / Arch / Vo:${oVol} ${oMute^^} / ${oDate} / "
 	sleep 3s
 done &
